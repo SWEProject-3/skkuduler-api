570be63f6197cff6158f651674ee5f5164c6da62
keemjoonsung <kjs990114@naver.com>
2024-10-04 23:43:15 +0900

    First Commit

---
+++ src/main/java/com/skku/skkuduler/SkkudulerApplication.java
@@ -0,0 +1,13 @@
+package com.skku.skkuduler;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkkudulerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SkkudulerApplication.class, args);
+    }
+
+}
